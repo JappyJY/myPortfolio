5f2a17630df835a3ea19fbaf4cb466a9abc950a6
JappyJY <lee.jiayu165@gmail.com>
2022-07-09 17:47:22 +0800

    updated design

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { NextScript, Head, Html, Main } from "next/document";
+
+const Document = () => {
+    return (
+        <Html className='dark' >
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+
+};
+
+export default Document;
